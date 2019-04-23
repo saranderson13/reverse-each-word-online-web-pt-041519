36596fd3982a05304e1fa918cdc8cd13e0737454
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 01:00:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+def reverse_each_word(string)
+  string.split.each { |word| word.reverse! }.join(" ")
+end
+
+def reverse_each_word(string)
+  string.split.collect { |word| word.reverse }.join(" ")
+end
