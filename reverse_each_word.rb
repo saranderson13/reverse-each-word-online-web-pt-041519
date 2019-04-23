037898bbc5fb420edef8d4f22846c79b920d7569
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 01:00:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,12 @@
 require 'pry'
 
 def reverse_each_word(string)
-  # array = string.split
   string.split.each { |word| word.reverse! }.join(" ")
-  # array.join(" ")
 end
 
-# def reverse_each_word(string)
-#   # binding.pry
-#   string.split.collect do |word| 
-#     word.reverse 
-#   end
-# end
+def reverse_each_word(string)
+  # binding.pry
+  string.split.collect do |word| 
+    word.reverse 
+  end
+end
